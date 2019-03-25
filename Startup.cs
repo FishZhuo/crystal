394989a162b6b9f3a21ef347e36b9271bbf2c83d
FishZhuo <15901898915@163.com>
2019-03-25 15:28:47 +0800

    “添加高亮方法”

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,7 @@ namespace ToDoApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSingleton<IToDoRepository,ToDoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
